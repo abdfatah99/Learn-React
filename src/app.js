049a692a2483edb9f5530746b4479f95099359a2
Abdul Fatah <fatahhamidah@gmail.com>
2022-10-02 23:34:52 +0700

    003 - event handling
    1. how to create an event using react
    2. what is the different between react and normal HTML
    3. how you can call function (an event) using argument

--- src/app.js
+++ src/app.js
@@ -1,5 +1,50 @@
 const root = document.querySelector("#root");
 
-const element = <div className="box"></div>;
+/**
+ * in normal HTML, you can use an event like this
+ * <button onclick="namaFunc()"></button>
+ * 
+ * what make different?
+ * HTML -> lowercase -> onclick="funcName()"
+ * React -> camelCase -> onClick={functionName}
+ *                               -> not using string ""
+ *                               -> using curly brackets,
+ *                               -> this because we call js function
+ * 
+ * when you insert the functionName to the react element, you don't 
+ * need to use parentheses, because using parentheses would execute
+ * the function immediately when you load the page, you don't want this. 
+ * You just want to execute the function if the function is called 
+ * using button
+ * 
+ * !THE PROBLEM
+ * What if we want to give an argument to the function?
+ * 1. using function
+ *      <button onClick={function() {
+ *          functionNames("argument")
+ *      }}>
+ *      click me
+ *      </button>
+ * 
+ * 2. using bind
+ *      <button onClick={functionNames.bind(this, "argument")}>
+ *          click me
+ *      </button>
+ *
+ *    if you console the -functionNames.bind(this, 'argument')-
+ *    it will create/produce new function with that argument
+ *    this keyword is used to referent the function (functionNames)
+ * 
+ * recap
+ *  1. define the event name
+ *  2. define the value -> function, you can abstraction the function, 
+ *                         or you can call immediately
+ */
+
+function buttonClick() {
+    alert("congratulation, you click the button");
+}
+
+const element = <button onClick={buttonClick}>click Me</button>;
 
 ReactDOM.render(element, root);
