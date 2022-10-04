4ff451538968cf296e615b20ff40b451677ea6cc
Abdul Fatah <fatahhamidah@gmail.com>
2022-10-04 20:35:25 +0700

    006-conditional-rendering
    this branch ans these question?
    - how to render a component based on certain condition?
    
    useful for login or rendering certain content to the page
    based on certain condition.

--- public/app.js
+++ public/app.js
@@ -1,116 +1,68 @@
 const root = document.querySelector("#root");
 /**
- * (def)
- * React state
- * state - data inside react component
- *       - isolated, can't accessed from another component
- *       - changeable, when you change the data, it will be
- *         re-rendered 
+ * CONDITIONAL RENDERING
  * 
- * then you have to create ReactComponent
- * (def)
- * React component: - function, start with Uppercase
- *                  - return, react element
- *     
- * (step - init) -> function 01
- * 1. create React component
- *     function App(){
- *         return <h1>React element</h1>;    
- *     }    
+ * : create a condition, while certain condition is fulfill, render certain
+ * : component, if the condition isn't fulfill, render optional component
  * 
- * what we want to do is create counter app, not calculator 
- * add number when you click + plus button
- * substract number when you click - minus button
+ * for example you create a login page, if the user is authorize, login page
+ * will direct to another page, if not, stay in the login page.
  * 
- * (step - 01)
- * 1. create simple app represent value using react state
- *     function App() {
- *         const state = React.useState(1);
- *         console.log(state)    
- *         
- *         return <h1>React Element</h1>
- *     }
+ * note: when you create a state inside component, while the state is change
+ *     : the component will be re-render 
  * 
- * what does React.useState(1) mean?
+ *
+
+ *=> function App() {
+ *=>     const [login, setLogin] = React.useState(false)
+
+ *=>     if(login) {
+ *=>         return (
+ *=>             <>
+ *=>             <h1>You already login</h1>
+ *=>             <button
+ *=>                 onClick={function() {
+ *=>                     setLogin(false)
+ *=>                 }}
+ *=>             >Logout</button>
+ *=>             </>
+ *=>         
+ *=>         )
+ *=>     }
+
+ *=>     return (
+ *=>         <>
+ *=>         <h1>Login!</h1>
+ *=>         <button
+ *=>             onClick={function() {
+ *=>                 setLogin(true)
+ *=>             }}
+ *=>         >Login</button>
+ *=>         </>
+ *=>     )
+ *=> }
+ *
+ */
+
+/**
+ * CONDITIONAL RENDERING INSIDE JSX
  * 
- * normally when you want to create this app using normal js,
- * you just create a variable then assign a value to calculate
- * the value using function (literal method) like:
- *     const state = 1
- *     
- *     function add(state) {
- *         state += 1
- *         return state; 
- *     }
- *     
- *     function substract(state) {...}
- * 
- * you will not do this with React, instead you'll assign the value
- * using React State.
- * 
- * when you console the state variable, this what you'll get:
- * (2) [1, ƒ]
- *     0: 1
- *     1: f()
- *         length: 1
- *         name: "bound dispatchSetState"
- *         arguments: (...)
- *         caller: (...)
- * 
- * you can see that we have two value:
- * first is the value (state) at the time
- * second is function, that you can use to update the data (state)
- * 
- * to change the state value, just call the function, then assign the
- * value
- * 
- * (step - 02)
+ * you can't use if else conditional inside JSX, so the alternative to do 
+ * that us use TERNARY OPERATOR
  * 
  */
-// function 01
-// function App() {
-//     return <h1>React element</h1>
-// }
-// step 01
-// function App() {
-//     const state = React.useState(1)
-//     console.log(state)
-//     return <h1>React Element</h1>
-// }
-// step 02
 
 function App() {
-  const state = React.useState(1); // get the value of the state at the time 
-
-  const count = state[0]; // update the value using dispatch function
-  // remember that state[1] is function from React useState
-
-  const updateCount = state[1]; // if you check the updateCount, it is the same as normal state
-
-  console.log(updateCount);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("button", {
+  const [login, setLogin] = React.useState(false);
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Application"), login ? /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("b", null, "You already login")), /*#__PURE__*/React.createElement("button", {
     onClick: function () {
-      updateCount(count - 1);
+      setLogin(false);
     }
-  }, "-"), /*#__PURE__*/React.createElement("span", null, count), /*#__PURE__*/React.createElement("button", {
+  }, "Logout")) : /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("i", null, "you are not login")), /*#__PURE__*/React.createElement("button", {
     onClick: function () {
-      updateCount(count + 1);
+      setLogin(true);
     }
-  }, "+"));
+  }, "Login")));
 }
-/**
- * there's another way to write to get more readable, using structuring
- * (extracting the useState array)
- * 
- * const [count, updateCount] = React.useState(0);
- * 
- * but, the code above is not general naming. The best practise code is
- * const [count, setCount] = React.useState(0); 
- * 
- * 
- */
-// because you are using jsx, you can directly bind the component
-// ? what does this mean
-
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -1,152 +1,88 @@
- const root = document.querySelector("#root");
+const root = document.querySelector("#root");
+
+/**
+ * CONDITIONAL RENDERING
+ * 
+ * : create a condition, while certain condition is fulfill, render certain
+ * : component, if the condition isn't fulfill, render optional component
+ * 
+ * for example you create a login page, if the user is authorize, login page
+ * will direct to another page, if not, stay in the login page.
+ * 
+ * note: when you create a state inside component, while the state is change
+ *     : the component will be re-render 
+ * 
+ *
+
+ *=> function App() {
+ *=>     const [login, setLogin] = React.useState(false)
+
+ *=>     if(login) {
+ *=>         return (
+ *=>             <>
+ *=>             <h1>You already login</h1>
+ *=>             <button
+ *=>                 onClick={function() {
+ *=>                     setLogin(false)
+ *=>                 }}
+ *=>             >Logout</button>
+ *=>             </>
+ *=>         
+ *=>         )
+ *=>     }
+
+ *=>     return (
+ *=>         <>
+ *=>         <h1>Login!</h1>
+ *=>         <button
+ *=>             onClick={function() {
+ *=>                 setLogin(true)
+ *=>             }}
+ *=>         >Login</button>
+ *=>         </>
+ *=>     )
+ *=> }
+ *
+ */
 
  /**
-  * (def)
-  * React state
-  * state - data inside react component
-  *       - isolated, can't accessed from another component
-  *       - changeable, when you change the data, it will be
-  *         re-rendered 
+  * CONDITIONAL RENDERING INSIDE JSX
   * 
-  * then you have to create ReactComponent
-  * (def)
-  * React component: - function, start with Uppercase
-  *                  - return, react element
-  *     
-  * (step - init) -> function 01
-  * 1. create React component
-  *     function App(){
-  *         return <h1>React element</h1>;    
-  *     }    
-  * 
-  * what we want to do is create counter app, not calculator 
-  * add number when you click + plus button
-  * substract number when you click - minus button
-  * 
-  * (step - 01)
-  * 1. create simple app represent value using react state
-  *     function App() {
-  *         const state = React.useState(1);
-  *         console.log(state)    
-  *         
-  *         return <h1>React Element</h1>
-  *     }
-  * 
-  * what does React.useState(1) mean?
-  * 
-  * normally when you want to create this app using normal js,
-  * you just create a variable then assign a value to calculate
-  * the value using function (literal method) like:
-  *     const state = 1
-  *     
-  *     function add(state) {
-  *         state += 1
-  *         return state; 
-  *     }
-  *     
-  *     function substract(state) {...}
-  * 
-  * you will not do this with React, instead you'll assign the value
-  * using React State.
-  * 
-  * when you console the state variable, this what you'll get:
-  * (2) [1, ƒ]
-  *     0: 1
-  *     1: f()
-  *         length: 1
-  *         name: "bound dispatchSetState"
-  *         arguments: (...)
-  *         caller: (...)
-  * 
-  * you can see that we have two value:
-  * first is the value (state) at the time
-  * second is function, that you can use to update the data (state)
-  * 
-  * to change the state value, just call the function, then assign the
-  * value
-  * 
-  * (step - 02)
+  * you can't use if else conditional inside JSX, so the alternative to do 
+  * that us use TERNARY OPERATOR
   * 
   */
 
-// function 01
-// function App() {
-//     return <h1>React element</h1>
-// }
-
-// step 01
-// function App() {
-//     const state = React.useState(1)
-//     console.log(state)
-
-//     return <h1>React Element</h1>
-// }
-
-// step 02
 function App() {
-    const state = React.useState(1);
-    
-    // get the value of the state at the time 
-    const count = state[0];
-
-    // update the value using dispatch function
-    // remember that state[1] is function from React useState
-    const updateCount = state[1];
-
-    // if you check the updateCount, it is the same as normal state
-    console.log(updateCount)
-
+    const [login, setLogin] = React.useState(false)
 
     return (
         <>
-            {/* 
-                to update the value, you have to call updateCount to the
-                button listener 
-                if you just add up like this
-                
-                <button onClick={function(){
-                    updateCount(-1)
-                }}>-</button>
-
-                it would result static number, not calculate up, just setup
-                the number to the assigned value.
-
-                to calculate up the value, you can use the current value
-                then add up the current with 1
-
-                <button onClick={function(){
-                    updateCount(count - 1)
-                }}>-</button>
-
-
-
-            */}
-            <button onClick={function(){
-                updateCount(count - 1)
-            }}>-</button>
-
-            <span>{count}</span>
-
-            
-            <button onClick={function(){
-                updateCount(count + 1)
-            }}>+</button>
+        <h1>Application</h1>
+        {/* basic idea
+            <p>{login ? "You already login" : "you are not login"}</p> 
+        */}
+        {login ?
+            <>
+            <p><b>You already login</b></p>
+            <button
+                onClick={function () {
+                    setLogin(false)
+                }} 
+            >Logout</button>
+            </>
+         :
+            <>
+            <p><i>you are not login</i></p>
+            <button
+                onClick={function(){
+                    setLogin(true)
+                }}
+            >Login</button>
+            </>
+            }
         </>
-    )
+   )
 }
 
-/**
- * there's another way to write to get more readable, using structuring
- * (extracting the useState array)
- * 
- * const [count, updateCount] = React.useState(0);
- * 
- * but, the code above is not general naming. The best practise code is
- * const [count, setCount] = React.useState(0); 
- * 
- * 
- */
-
-// because you are using jsx, you can directly bind the component
-// ? what does this mean
-ReactDOM.render(<App/>, root);
+ReactDOM.render(<App />, root)
