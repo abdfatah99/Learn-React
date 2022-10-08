caa75d6822b894afc4fc5f2eb809d20e00d29b1c
Abdul Fatah <fatahhamidah@gmail.com>
2022-10-08 08:43:09 +0700

    test

--- src/app.js
+++ src/app.js
@@ -1,7 +1,61 @@
 const root = document.querySelector("#root");
 
-function App(){
-    return <h1>Hello this is initialization of React list key render multiple component</h1>
+/**
+ * Making list using normal HTML list element
+ * 
+ * it's quite simple, you just create normal HTML list the insert it into JSX
+ */
+
+// function App() {
+//     return (
+//         <ul>
+//             <li>Apple</li>
+//             <li>Grape</li>
+//             <li>Pineaple</li>
+//             <li>Orange</li>
+//         </ul>
+//     )
+// }
+
+/**
+ * List element using Object
+ * 
+ * This part is userful in future if you want to get data from database or 
+ * external API.
+ */
+
+function App() {
+    const fruits = ['Apple', 'Grape', 'Pineaple', 'Orange']
+
+    return (
+        <ul>
+            {/* if you just call the element like this
+                
+                { 
+                    fruits.map(function(fruit) {
+                        return <li>{fruit}</li>
+                    })
+                } 
+                
+                it will cause a warning that each element should have a key
+                to define the uniqueness.
+                In the future or production, you can use like product id to
+                define the key. 
+                - each key should unique, other than that it will cause an error
+
+                why we use map?
+
+                because we want to implement return statement, if you use forEach, 
+                it wouldn't work, because it's not support.
+            */}
+
+            {
+                fruits.map(function(fruit) {
+                    return <li key={fruit}>fruit</li>
+                })
+            }
+        </ul>
+    )
 }
 
 ReactDOM.render(<App />, root)
