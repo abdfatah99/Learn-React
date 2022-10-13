a244cd122e990a79b4c4b9def8b4769f4c6987ac
Abdul Fatah <fatahhamidah@gmail.com>
2022-10-13 16:24:03 +0700

    start with react form

--- public/app.js
+++ public/app.js
@@ -1,2 +1,44 @@
 const root = document.querySelector("#root");
+/**
+ * create react form like normal form
+ */
+// function App(){
+//   function ketikadisubmit(event){
+//     // stop from submission
+//     event.preventDefault()
+//     // check if the function is run
+//     console.log("the button clicked")
+//   }
+//   return(
+//     <form onSubmit={ketikadisubmit}>
+//       <div>
+//         <label>Nama: </label>
+//         <input type="text" name="nama" />
+//       </div>
+//       <button type="submit">Submit</button>
+//     </form>
+//   )
+// }
+
+/**
+ * How to the data from the form-input element into the console
+ */
+
+function App() {
+  function onSubmitFunc(event) {
+    event.preventDefault();
+    console.log('the button is clicked');
+  }
+
+  return /*#__PURE__*/React.createElement("form", {
+    onSubmit: onSubmitFunc
+  }, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", {
+    htmlFor: "name"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "name",
+    id: "name"
+  })), /*#__PURE__*/React.createElement("button", null, "Submit"));
+}
+
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -1,4 +1,49 @@
 const root = document.querySelector("#root");
 
+/**
+ * create react form like normal form
+ */
+
+// function App(){
+//   function ketikadisubmit(event){
+//     // stop from submission
+//     event.preventDefault()
+
+//     // check if the function is run
+//     console.log("the button clicked")
+
+//   }
+
+//   return(
+//     <form onSubmit={ketikadisubmit}>
+//       <div>
+//         <label>Nama: </label>
+//         <input type="text" name="nama" />
+//       </div>
+//       <button type="submit">Submit</button>
+//     </form>
+//   )
+// }
+
+/**
+ * How to the data from the form-input element into the console
+ */
+
+function App(){
+  function onSubmitFunc(event){
+    event.preventDefault() 
+    console.log('the button is clicked')
+  }
+
+  return (
+    <form onSubmit={onSubmitFunc}>
+      <div>
+        <label htmlFor='name'></label>
+        <input type="text" name='name' id='name' />
+      </div>
+      <button>Submit</button>
+   </form>
+  )
+}
 
 ReactDOM.render(<App />, root)
