980ded1f7ce2d34445b40c3e0a626fd56c319b01
Abdul Fatah <fatahhamidah@gmail.com>
2023-01-15 10:32:48 +0700

    review
    commit concern
    	- restructure html
    	- review src/app.js

--- public/app.js
+++ public/app.js
@@ -53,62 +53,62 @@ const root = document.querySelector("#root");
 
 /**
  * REACT USE EFFECT
- * 
+ *
  * code: React.useEffect()
  * - useEffect function will executed while the component is rendered.
- * 
+ *
  *=> function App() {
  *=>   React.useEffect(function () {
  *=>       console.log("rendered")
  *=>       console.log(document.getElementById("title"))
  *=>    })
- *=>    
+ *=>
  *=>    return <h1 id="title">Hello this is title</h1>
  *=> }
  *
  * even we delay set the rendered function, we can still access the element
- * 
+ *
  *=> setTimeout(function (){
  *=>    ReactDOM.render(<App />, root)
  *=> }, 1000)
  */
 
 /**
- * RE-EXECUTE useEffect WHILE a state is change
- * 
- * the thing that you have to keep in mind is when you re-render (for example
- * there's a change in the state) the component, this function will also 
- * re-execute.
- * 
- * for example you have a button that change a state
- * 
- * then you can check in the console that the function (useEffect) is
- * re-excuted
- *
+  * RE-EXECUTE useEffect WHILE a state is change
+  * 
+  * the thing that you have to keep in mind is when you re-render (for example
+  * there's a change in the state) the component, this function will also 
+  * re-execute.
+  * 
+  * for example you have a button that change a state
+  * 
+  * then you can check in the console that the function (useEffect) is
+  * re-excuted
+  *
 
- *=> function App() {
- *=>   const [clicked, setClicked] = React.useState(false)
- *=>   console.log(clicked)
+  *=> function App() {
+  *=>   const [clicked, setClicked] = React.useState(false)
+  *=>   console.log(clicked)
 
- *=>   React.useEffect(function() {
- *=>       console.log(document.getElementById("title"))
- *=>   })
- *=>   
- *=>   return (
- *=>       <>
- *=>           <h1 id="title">Hello this is Title</h1>
- *=>           <button
- *=>               onClick={function() {
- *=>                   setClicked(true)
- *=>               }}
- *=>           >Click Me!</button>
- *=>       </>
- *=>   )
- *=> }
- *
- * keep in mind that React.useEffect is watch all state, the case above is just
- * one state (clicked).
- */
+  *=>   React.useEffect(function() {
+  *=>       console.log(document.getElementById("title"))
+  *=>   })
+  *=>   
+  *=>   return (
+  *=>       <>
+  *=>           <h1 id="title">Hello this is Title</h1>
+  *=>           <button
+  *=>               onClick={function() {
+  *=>                   setClicked(true)
+  *=>               }}
+  *=>           >Click Me!</button>
+  *=>       </>
+  *=>   )
+  *=> }
+  *
+  * keep in mind that React.useEffect is watch all state, the case above is just
+  * one state (clicked).
+  */
 
 /**
  * SECOND ARGUMENT OF USE EFFECT
@@ -169,35 +169,35 @@ const root = document.querySelector("#root");
  *   is already rendered or not.
  * - using useEffect also can be use to fetch data
  * - using useEffect also can be use to initialize a module or library
- *   
+ *
  *   if we already know that the component is already rendered, we can track
- *   our code of using the library. 
+ *   our code of using the library.
  *   we need initialization once at the beginning, it's handy using an empty
  *   array at the second argument.
- * 
+ *
  * PROBLEM WITH INITIALIZE A LIBRARY
  *
  * if you already initialize a library at the first time, the library
- * is saved in the memory. But, when you change or move to another component 
+ * is saved in the memory. But, when you change or move to another component
  * (another page) then you go back to this component, the instance from this
- * library (library that the first time you initialize) would still in the 
+ * library (library that the first time you initialize) would still in the
  * memory. THE LIBRARY IS STILL ALIVE AND NOT DELETED.
- * 
+ *
  * HOW TO SOLVE THE PROBLEM? (clean up or destroy)
- * some library has thier own method to clean up or destroy thier instance, 
+ * some library has thier own method to clean up or destroy thier instance,
  * to execute destroying the instance, we can call that function or method
- * while the component is un-mount. 
- * 
+ * while the component is un-mount.
+ *
  * mount    : component rendered (at the first time) into DOM tree
  * un-mount : component deleted from the DOM tree
- * 
+ *
  * to do this, just call in the return expression
- * 
+ *
  * example
- * 
+ *
  *=> React.useEffect(function() {
  *=>    console.log('init carousel')
- *=>    
+ *=>
  *=>    return function () { // this function will executed while the component
  *=>                         // is deleted from DOM tree (un-mount)
  *=>                         // or while the component is re-render (state change)
@@ -207,7 +207,32 @@ const root = document.querySelector("#root");
  *
  * when you go back to this component, the library will start to re-initialize
  * like at the first time.
- * 
+ *
  */
+// src: https://www.youtube.com/watch?v=iftKirX0kD8
+
+function App() {
+  const [showComponent, setShowComponent] = React.useState(false);
+  return /*#__PURE__*/React.createElement("div", {
+    className: "text-center"
+  }, /*#__PURE__*/React.createElement("h3", {
+    className: ""
+  }, "Applying return statement of useEffect (Component lifecycle)"), /*#__PURE__*/React.createElement("button", {
+    className: "btn btn-success",
+    onClick: () => {
+      setShowComponent(prev => !prev);
+    }
+  }, "Toggle Component"), showComponent && /*#__PURE__*/React.createElement(SayComponent, null));
+}
+
+const SayComponent = () => {
+  React.useEffect(() => {
+    console.log("Mount, Text 'hey' appear");
+    return () => {
+      console.log("unMount, Text 'hey' dissappear");
+    };
+  }, []);
+  return /*#__PURE__*/React.createElement("h2", null, "Hey");
+};
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -54,27 +54,27 @@ const root = document.querySelector("#root");
 
 /**
  * REACT USE EFFECT
- * 
+ *
  * code: React.useEffect()
  * - useEffect function will executed while the component is rendered.
- * 
+ *
  *=> function App() {
  *=>   React.useEffect(function () {
  *=>       console.log("rendered")
  *=>       console.log(document.getElementById("title"))
  *=>    })
- *=>    
+ *=>
  *=>    return <h1 id="title">Hello this is title</h1>
  *=> }
  *
  * even we delay set the rendered function, we can still access the element
- * 
+ *
  *=> setTimeout(function (){
  *=>    ReactDOM.render(<App />, root)
  *=> }, 1000)
  */
 
- /**
+/**
   * RE-EXECUTE useEffect WHILE a state is change
   * 
   * the thing that you have to keep in mind is when you re-render (for example
@@ -170,35 +170,35 @@ const root = document.querySelector("#root");
  *   is already rendered or not.
  * - using useEffect also can be use to fetch data
  * - using useEffect also can be use to initialize a module or library
- *   
+ *
  *   if we already know that the component is already rendered, we can track
- *   our code of using the library. 
+ *   our code of using the library.
  *   we need initialization once at the beginning, it's handy using an empty
  *   array at the second argument.
- * 
+ *
  * PROBLEM WITH INITIALIZE A LIBRARY
  *
  * if you already initialize a library at the first time, the library
- * is saved in the memory. But, when you change or move to another component 
+ * is saved in the memory. But, when you change or move to another component
  * (another page) then you go back to this component, the instance from this
- * library (library that the first time you initialize) would still in the 
+ * library (library that the first time you initialize) would still in the
  * memory. THE LIBRARY IS STILL ALIVE AND NOT DELETED.
- * 
+ *
  * HOW TO SOLVE THE PROBLEM? (clean up or destroy)
- * some library has thier own method to clean up or destroy thier instance, 
+ * some library has thier own method to clean up or destroy thier instance,
  * to execute destroying the instance, we can call that function or method
- * while the component is un-mount. 
- * 
+ * while the component is un-mount.
+ *
  * mount    : component rendered (at the first time) into DOM tree
  * un-mount : component deleted from the DOM tree
- * 
+ *
  * to do this, just call in the return expression
- * 
+ *
  * example
- * 
+ *
  *=> React.useEffect(function() {
  *=>    console.log('init carousel')
- *=>    
+ *=>
  *=>    return function () { // this function will executed while the component
  *=>                         // is deleted from DOM tree (un-mount)
  *=>                         // or while the component is re-render (state change)
@@ -208,7 +208,42 @@ const root = document.querySelector("#root");
  *
  * when you go back to this component, the library will start to re-initialize
  * like at the first time.
- * 
+ *
  */
 
-ReactDOM.render(<App />, root)
+// src: https://www.youtube.com/watch?v=iftKirX0kD8
+
+function App() {
+  const [showComponent, setShowComponent] = React.useState(false);
+
+  return (
+    <div className="text-center">
+      <h3 className="">
+        Applying return statement of useEffect (Component lifecycle)
+      </h3>
+      <button
+        className="btn btn-success"
+        onClick={() => {
+          setShowComponent((prev) => !prev);
+        }}
+      >
+        Toggle Component
+      </button>
+      {showComponent && <SayComponent />}
+    </div>
+  );
+}
+
+const SayComponent = () => {
+  React.useEffect(() => {
+    console.log("Mount, Text 'hey' appear")
+  
+    return () => {
+      console.log("unMount, Text 'hey' dissappear")
+    }
+  }, [])
+  
+  return <h2>Hey</h2>
+}
+
+ReactDOM.render(<App />, root);
