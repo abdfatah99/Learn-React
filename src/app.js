41280a37c51771a78758e4bf7c9b5014d93b68f9
Abdul Fatah <fatahhamidah@gmail.com>
2024-03-01 23:11:17 +0700

    check different type of how to render function component

--- src/app.js
+++ src/app.js
@@ -47,16 +47,62 @@ function buttonClick(stdIndentity) {
     alert(`congratulation, ${name} click the button`);
 }
 
+const ButtonHandlerBind = (
+    <button onClick={buttonClick.bind(this, "fatah")}>Click Me</button>
+);
+
 // ======================= Adding Interactivity ================================
+// remember that this file using React.createElement() function to create element
+// you can't directory create Function Component
+// later you have to practice it using Function Component
+
+// list of component that you have to create
+// 1. Button { element: <button>, children: { children }}
+// 2. Toolbar { element: Fragment { children: [Button, Button] } }
+// 3. MainApp { element: Toolbar}
+
+/**
+ * The button only receive and run the function of onClick handler.
+ *
+ * Notes:
+ * 1. This Button Component is a function
+ * 2. To be able to call this function component as a component, you need to
+ *    invoke the function.
+ * 
+ * Functional component of Button
+ * -> function Button({ onClick, children }){
+ * ->   return (
+ * ->       <button onClick={onClick}>{children}</button>
+ * ->   )
+ * -> }
+ *
+ * @param {htmlOnClick} onClick - Click event handler
+ * @returns {JSX.Element}
+ */
+
+const Button = ({ onClick, children }) =>
+    React.createElement(
+        "button",
+        {
+            onClick: onClick,
+        },
+        children
+    );
+
+// const MainApp = React.createElement();
 
 function MainApp() {
-    return (
-        // <Toolbar
-        //     postMessage={() => alert("Posting a message")}
-        //     uploadImage={() => alert("Upload an image")}
-        // />
-        <div>Test</div>
-    );
+    // return (
+    //     // <Toolbar
+    //     //     postMessage={() => alert("Posting a message")}
+    //     //     uploadImage={() => alert("Upload an image")}
+    //     // />
+    // );
+
+    Toolbar({
+        postMesage: () => alert("Posting a message"),
+        uploadImage: () => alert("Upload an Image"),
+    });
 }
 
 /**
@@ -65,36 +111,22 @@ function MainApp() {
  * @param {ClickHandler} handler - The clik handler for Post Message
  * @param {ClickHandler} handler - The clik handler for Upload Image
  */
-function Toolbar() {
-    return (
-        <>
-            {/* <Button>Post Message</Button>
-            <Button>Upload Image</Button> */}
-            <div>Test</div>
-        </>
+function Toolbar({ postMessage, uploadImage }) {
+    // return (
+    //     <>
+    //         <Button onClick={postMessage}>Post Message</Button>
+    //         <Button onClick={uploadImage}>Upload Image</Button>
+    //     </>
+    // );
+
+    return React.createElement(
+        React.Fragment,
+        {},
+        Button({ onClick: postMessage, children: "Post Message" }),
+        Button({ onClick: uploadImage, children: "Upload Image" })
     );
 }
 
-/**
- * The button only receive and run the function of onClick handler.
- * 
- * Notes: 
- * 1. This Button Component is a function
- * 2. To be able to call this function component as a component, you need to 
- *    invoke the function.
- *
- * @param {htmlOnClick} onClick - Click event handler
- * @returns {JSX.Element}
- */
-// function Button({ onClick, children }) {
-//     return (
-//         <button
-//         >
-//             {children}
-//             {console.log("testing")}
-//         </button>
-//     );
-// }
 
 /**
  * Because i want to render all component:
@@ -104,12 +136,9 @@ function Toolbar() {
  * I have to use Fragment from react to make it able to render multiple
  * component at the same time without wrapping it using another tag.
  */
-// const ButtonHandlerBind = (
-//     <button onClick={buttonClick.bind(this, "fatah")}>Click Me</button>
-// );
 
 const element = React.createElement(React.Fragment, {
-    children: [MainApp],
+    children: [ButtonHandlerBind],
 });
 
 ReactDOM.render(element, root);
